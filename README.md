7945bf9209baa62326b6b557c3c6191f92f23393
Mike Erickson <codedungeon@gmail.com>
2020-01-11 14:35:23 -0800

    the beginning

---
+++ README.md
@@ -0,0 +1,24 @@
+# <project_name>
+
+## Description
+
+** Project Description **
+
+## Usage
+
+** Describe module functions **
+
+### License
+
+Copyright &copy; 2019 Mike Erickson
+Released under the MIT license
+
+## Credits
+
+<project_name> written by Mike Erickson
+
+E-Mail: [codedungeon@gmail.com](mailto:codedungeon@gmail.com)
+
+Twitter: [@codedungeon](http://twitter.com/codedungeon)
+
+Website: [codedungeon.io](http://codedungeon.io)
---
+++ scripts/colors.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+### SAMPLE
+# printf "\n ${COL_GREEN}[ •• ]${COL_RESET} ${COL_YELLOW} ==> Copying files to ${COL_CYAN}./examples${COL_GREEN} ...   ${COL_RESET}\n"
+###
+
+# Colors used for status updates
+ESC_SEQ="\x1b["
+COL_RESET=${ESC_SEQ}"39;49;00m"
+COL_RED=${ESC_SEQ}"0;31m"
+COL_LRED=${ESC_SEQ}"31;01m"
+COL_GREEN=${ESC_SEQ}"0;32m"
+COL_LGREEN=${ESC_SEQ}"32;01m"
+COL_YELLOW=${ESC_SEQ}"0;33m"
+COL_LYELLOW=${ESC_SEQ}"33;01m"
+COL_BLUE=${ESC_SEQ}"0;34m"
+COL_LBLUE=${ESC_SEQ}"34;01m"
+COL_MAGENTA=${ESC_SEQ}"0;35m"
+COL_LMAGENTA=${ESC_SEQ}"35;01m"
+COL_CYAN=${ESC_SEQ}"0;36m"
+COL_LCYAN=${ESC_SEQ}"36;01m"
+COL_WHITE=${ESC_SEQ}"0;37m"
+COL_WHITE=${ESC_SEQ}"37;01m"
